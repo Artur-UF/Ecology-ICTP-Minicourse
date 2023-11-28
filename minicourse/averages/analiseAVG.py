453d89cfcab83ac199855fa4525dd5a7c44a099e
Artur Uhlik Frohlich <artur.uhlik@gmail.com>
2023-11-28 20:47:18 -0300

    Coisinhas

--- minicourse/averages/analiseAVG.py
+++ minicourse/averages/analiseAVG.py
@@ -13,8 +13,10 @@ numB = np.loadtxt(f'avg_rhoa_{rhoa:.2f}_gb_{gb:.2f}.dat', unpack=True)
 numB = np.asarray(numB)
 numA = N - numB 
 
-plt.plot(numA, label='Big')
-plt.plot(numB, label='Small')
+plt.plot(numA, label=r'$N_{A}$ '+'(Big)')
+plt.plot(numB, label=r'$N_{B}$'+'(Small)')
 plt.legend()
+plt.xlabel('t')
+plt.ylabel(r'$\left \langle N \right \rangle$')
 plt.title(r'$\rho_{A} = $'+f'{rhoa}'+r' $\mid g_{B} = $'+f'{gb}')
 plt.savefig('avgnums.png', dpi=400)
